12864ac7d79d1e729edb4c85799ac09e0828f0e9
Hanna Kirieieva <kireevaann2006@gmail.com>
2025-03-09 20:56:35 +0100

    New feature-max implemented

--- project1/project1/Program.cs
+++ project1/project1/Program.cs
@@ -24,3 +24,26 @@ public class Calculator
         return sum / numbers.Length;
     }
     
+    class Calculator
+    {
+        // Static method to return the maximum value in an array of integers
+        public static int FindMax(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be null or empty");
+            }
+
+            int max = numbers[0]; // Assume the first element is the largest initially
+
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number; // Update the max value when a larger number is found
+                }
+            }
+
+            return max;
+        }
+    }
