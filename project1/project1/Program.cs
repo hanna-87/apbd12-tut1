7f26d44d7bdd9b0bd4d7c4fbcfa59b1e878e7692
Hanna Kirieieva <kireevaann2006@gmail.com>
2025-03-09 20:49:47 +0100

    Implement static method to calculate average of an array of ints

--- project1/project1/Program.cs
+++ project1/project1/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 Console.WriteLine("Hello, World!");
 Console.WriteLine("You can do it!")
+public class Calculator
+{
+    // Static method to calculate the average of an array of integers
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty");
+        }
+
+        double sum = 0;
+
+        // Loop through the array to calculate the sum of the numbers
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        // Calculate and return the average
+        return sum / numbers.Length;
+    }
+    
